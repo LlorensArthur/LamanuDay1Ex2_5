9c338fe9b56fbe5af3fa394cf91f64f09b17d3bf
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:16:52 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Indiquez un mois parmi : janvier, février, mars, avril, mai, juin, juillet, août, septembre, octobre, novembre et décembre");
+string mois = Console.ReadLine();
+if (mois == "décembre"
+    || mois == "janvier"
+    || mois == "février")
+{
+    Console.WriteLine("La saison du mois saisi est l'HIVER.");
+}
+if (mois == "mars"
+    || mois == "avril"
+    || mois == "mai")
+{
+    Console.WriteLine("La saison du mois saisi est le PRINTEMPS.");
+}
+if (mois == "juin"
+    || mois == "juillet"
+    || mois == "aout")
+{
+    Console.WriteLine("La saison du mois saisi est l'ÉTÉ.");
+}
+if (mois == "septembre"
+    || mois == "octobre"
+    || mois == "novembre")
+{
+    Console.WriteLine("La saison du mois saisi est l'AUTOMNE.");
+}
